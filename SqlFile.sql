055e5742586462fdd33c4d964c639dc1b873e6c0
Aniket <aniketsingh0204@gmail.com>
2022-11-29 00:30:03 +0530

    add sql file

---
+++ SqlFile.sql
@@ -0,0 +1,9 @@
+//creating database
+create database quadwave;
+
+//use data base
+use quadwave;
+
+//create table
+
+create employee_detail(EId int, EName varchar(20),DOJ Date,YOE int,Designation varchar(20));
